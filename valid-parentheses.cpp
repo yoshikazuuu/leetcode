0266f1fdef90664d4ba23b55f869688b434a2c00
yoshikazuuu <febrianojerry@gmail.com>
2023-09-10 16:24:55 +0700

    first commit

---
+++ valid-parentheses.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+  bool isValid(string s) {
+    stack<char> stack;
+    for (auto i : s) {
+      if (stack.empty()) {
+        stack.push(i);
+      } else if ((i == ')' && stack.top() == '(') ||
+                 (i == ']' && stack.top() == '[') ||
+                 (i == '}' && stack.top() == '{')) {
+        stack.pop();
+      } else {
+        stack.push(i);
+      }
+    }
+
+    return stack.empty();
+  }
+};
