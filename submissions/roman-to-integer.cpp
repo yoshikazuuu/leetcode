0521413331e3880b45d88c9b31a62e0fd35233da
yoshikazuuu <febrianojerry@gmail.com>
2023-09-18 14:43:24 +0700

    https://leetcode.com/problems/roman-to-integer

---
+++ submissions/roman-to-integer.cpp
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+#define ll long long
+#define INF 1000000000000000003
+#define MOD 1e9 + 7
+
+#define f(i, a, b) for (int i = a; i < b; i++)
+#define fz(i, a) f(i, 0, a)
+#define ffz(i, j, a, b) f(i, 0, a) f(j, 0, b)
+
+#define vi(name, type, n) vector<type> name(n, 0);
+#define vin(name, n) fz(i, n) cin >> name[i];
+#define vout(name, n) fz(i, n) cout << name[i] << " ";
+
+class Solution {
+public:
+  int romanToInt(string s) {
+    unordered_map<char, int> m;
+    int ans = 0;
+
+    m['I'] = 1;
+    m['V'] = 5;
+    m['X'] = 10;
+    m['L'] = 50;
+    m['C'] = 100;
+    m['D'] = 500;
+    m['M'] = 1000;
+
+    fz(i, s.length())(m[s[i]] < m[s[i + 1]]) ? ans -= m[s[i]] : ans += m[s[i]];
+
+    return ans;
+  }
+};
